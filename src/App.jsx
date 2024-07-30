5a955b2d924185f219900d41bc35b7c6f4833f2c
ay-nam <aynamfanvar24@gmail.com>
2024-07-31 00:38:44 +0530

    kkk

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <h1>anandaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaapotttttttttttttttttttttttttttttttttttttttttttttttttttyyyyyyyyyyyyyyyyyyyyy </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          hiiii caryyy boi {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
